0e966881d0efa5268f0bc41d6d7cad25c1f462e6
Abdelrahman Mohammed <abdo.moh96@outlook.com>
2025-08-23 06:24:47 +0300

    added all empty pages

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,19 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        contact: resolve(__dirname, 'contact.html'),
-        vacancies: resolve(__dirname, 'vacancies.html'),
+        main: resolve(__dirname, 'index.html'), //======> done
+        about: resolve(__dirname, 'about.html'),
+        album: resolve(__dirname, 'album.html'),
+        consultancy_services: resolve(__dirname, 'consultancy_services.html'),
+        training_and_workshop: resolve(__dirname, 'training_and_workshop.html'),
+        professional_services: resolve(__dirname, 'professional_services.html'),
+        staff_augmentation: resolve(__dirname, 'staff_augmentation.html'),
+        vacancies: resolve(__dirname, 'vacancies.html'), //======> done
+        submit_your_cv: resolve(__dirname, 'submit_your_cv.html'),
+        markets_we_serve: resolve(__dirname, 'markets_we_serve.html'),
+        contact: resolve(__dirname, 'contact.html'), //======> done
+
+
       },
     },
   },
