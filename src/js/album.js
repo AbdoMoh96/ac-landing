f6e5cbdd89c34b753228f97861276c892801a6e3
Abdelrahman Mohammed <abdo.moh96@outlook.com>
2025-08-24 15:13:33 +0300

    swiper done

--- src/js/album.js
+++ src/js/album.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".mySwiper", {
+let swiper = new Swiper(".mySwiper", {
     lazy: true,
     slidesPerView: 4,
     spaceBetween: 45,
@@ -11,3 +11,23 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+let swiperModal = new Swiper(".modalSwiper", {
+    lazy: true,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: ".swiper-button-next-modal",
+        prevEl: ".swiper-button-prev-modal",
+    },
+});
+
+let modal = document.getElementById("slider-modal");
+
+let openModal = (slideId= 1) => {
+   swiperModal.slideTo(slideId, 0);
+   modal.classList.toggle("invisible");
+}
+
+let closeModal = () => {
+    modal.classList.toggle("invisible");
+}
