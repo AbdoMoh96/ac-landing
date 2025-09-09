176b9d4b79cfe1e7e6e466f3830a0fa1286063f5
Abdelrahman Mohammed <abdo.moh96@outlook.com>
2025-09-09 03:27:12 +0300

    fixed markets we serve customer slider

--- public/js/markets-we-serve.js
+++ public/js/markets-we-serve.js
@@ -41,8 +41,9 @@ const businessSectorsSwiper = new Swiper(".business-sectors", {
 });
 
 const customersSwiper = new Swiper(".customers", {
-    // your base options
+// your base options
     lazy: { loadOnTransitionStart: true },
+    slidesPerView: 6,
     loop: true,
     autoplay: {
         delay: 2000,
@@ -56,7 +57,8 @@ const customersSwiper = new Swiper(".customers", {
     breakpoints: {
         0:   { slidesPerView: 1 },
         640: { slidesPerView: 2 },
-        700: { slidesPerView: 3 },
-        1024:{ slidesPerView: 4 }
+        700: { slidesPerView: 4 },
+        800: { slidesPerView: 6 },
+        2000:{ slidesPerView: 6 }
     }
 });
