b385148f4dc838a2f3722effbc673d9e3e1b935d
Abdelrahman Mohammed <abdo.moh96@outlook.com>
2025-09-15 07:58:33 +0300

    shared footer and header created

---
+++ public/js/confirmation-modal.js
@@ -0,0 +1,9 @@
+let modal = document.getElementById("confirmation-modal");
+
+const showConfirmationModal = () => {
+    modal.classList.toggle("invisible");
+}
+
+const closeConfirmationModal = () => {
+    modal.classList.toggle("invisible");
+}
